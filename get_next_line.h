95702c4e47c6783b2fbcb9a9003ca3808bd41341
Pablo Soto <pabletesglez@gmail.com>
2021-11-07 23:08:41 +0100

    init

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 23:03:09 by psoto-go          #+#    #+#             */
+/*   Updated: 2021/11/07 23:05:54 by psoto-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <limits.h>
+
+char    *get_next_line(int fd);
+
+#endif
